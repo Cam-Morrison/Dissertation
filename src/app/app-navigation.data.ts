643d62eef27a30c12f1d83158ed48e837af24afa
Cam-morrison <cam-morrison@outlook.com>
2022-08-15 16:04:14 +0100

    Implemented navigation bar

---
+++ src/app/app-navigation.data.ts
@@ -0,0 +1,27 @@
+export const navigationData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'dashboard',
+        label: 'Dashboard'
+    },
+    {
+        routeLink: 'news',
+        icon: 'library_books',
+        label: 'Market News'
+    },
+    {
+        routeLink: '',
+        icon: 'trending_up',
+        label: 'Search Stocks'
+    },
+    {
+        routeLink: '',
+        icon: 'memory',
+        label: 'Predictive Learning'
+    },
+    {
+        routeLink: '',
+        icon: 'account_box',
+        label: 'My Account'
+    }
+]
