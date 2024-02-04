e0fae2b117183726306b11d3a84357ff0bdb2c28
Cameron Morrison <Cam-morrison@outlook.com>
2024-02-04 10:25:09 +0000

    Update SecretsManagerService.cs

--- backend/Services/SecretsManagerService/SecretsManagerService.cs
+++ backend/Services/SecretsManagerService/SecretsManagerService.cs
@@ -10,14 +10,14 @@ namespace backend.services
         readonly string clientSecret;
     
         public SecretsManagerService() {
-            tenantId = "cc0332ed-edc4-4fe2-b412-ae14ffdcefba";
-            clientId = "2de3bdbd-4813-4edb-ad7b-5ccab9a480ed";
-            clientSecret = "YoQ8Q~oYIkHyx2uTlK_g2fSJJ_XJXm~jDYny8aLi";     
+            tenantId = "REPLACE";
+            clientId = "REPLACE";
+            clientSecret = "REPLACE";     
         }
 
         public string GetSecret(string SecretName) {
             var credentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
-            var client = new SecretClient(vaultUri: new Uri("https://investmentdashboard.vault.azure.net/"), credentials);
+            var client = new SecretClient(vaultUri: new Uri("REPLACE"), credentials);
             return client.GetSecret(SecretName).Value.Value;
         }
     }
